2cc8a86d27b40a9d9a70897c6c602d57f4a94376
Gopi Nath <69713591+gns34@users.noreply.github.com>
2025-04-29 23:31:20 +0530

    add city input form to getCity class in forms.py

--- demox/ukweather/forms.py
+++ demox/ukweather/forms.py
@@ -1,1 +1,12 @@
 from django import form
+
+class getCity(forms.Form):
+    cityName=forms.CharField(
+        label='Enter a city',
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'placeholder' :'London, Amsterdam',
+            'class':'form-control' 
+        })
+
+    )
